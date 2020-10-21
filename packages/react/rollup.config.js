f95443c9a9d15e813c595edf0525ad7bee53f569
Adam Pietrasiak <adam@pietrasiak.com>
2020-10-21 22:47:01 +0200

    preparing react package for build

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,11 +1,13 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
-export default {
-  input: './lib/index.ts',
-  output: {
-    file: './lib/index.js',
-    format: 'cjs',
+export default [
+  {
+    input: './lib/index.ts',
+    output: {
+      file: './lib/index.js',
+      format: 'cjs',
+    },
+    plugins: [typescript({ useTsconfigDeclarationDir: true })],
+    external: ['tslib', 'react', 'react-dom', 'statek'],
   },
-  plugins: [typescript()],
-  external: ['tslib', 'react', 'react-dom', 'statek'],
-};
+];
